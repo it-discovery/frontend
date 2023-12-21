176c8418d9e6c989b7b5fd51e4dc73a4eb4d163e
Sergiy <sergey.morenets@gmail.com>
2023-12-21 14:34:20 +0200

    Task 4

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,58 @@
+'use strict';
+
+class Book {
+    constructor(title, author, publisher, pages, year) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.pages = pages;
+        this.year = year;
+    }
+}
+
+class BookStorage {
+    static #instance;
+
+    books = [];
+
+    static {
+        this.#instance = new BookStorage();
+    }
+
+    static getInstance() {
+        return this.#instance;
+    }
+
+    addBook(title, author, publisher, pages, year) {
+        this.books.push(new Book(title, author, publisher, pages, year));
+    }
+
+    displayBooks() {
+        let text = '';
+        for (let book of this.books) {
+            text += `title: ${book.title}, author: ${book.author}`;
+        }
+        updateHTML('bookList', text);
+
+    }
+}
+
+class Author {
+
+}
+
+class Order {
+
+}
+
+class Publisher {
+
+}
+
+class User {
+
+}
+
 function getInputValue(elementId) {
 
     const inputElement = document.getElementById(elementId);
@@ -10,5 +65,13 @@ function updateHTML(elementId, value) {
     inputElement.innerHTML = value;
 }
 
-document.getElementById('').addEventListener('click', () => {
+document.getElementById('addBook').addEventListener('click', () => {
+    const title = getInputValue('inputTitle');
+    const publisher = getInputValue('inputPublisher');
+    const pages = getInputValue('inputPages');
+    const author = getInputValue('inputAuthor');
+    const year = getInputValue('inputYear');
+
+    BookStorage.getInstance().addBook(title, author, publisher, pages, year);
+    BookStorage.getInstance().displayBooks();
 });
