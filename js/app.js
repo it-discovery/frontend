b999923757da05c5b28d9f6f368add0c4c40ec15
Sergiy <sergey.morenets@gmail.com>
2023-12-21 15:24:40 +0200

    Task 5.2. Use ES6 modules

---
+++ js/app.js
@@ -0,0 +1,71 @@
+import {getInputValue, updateHTML} from "./utils";
+
+class Book {
+    constructor(title, author, publisher, pages, year) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.pages = pages;
+        this.year = year;
+    }
+}
+
+class BookStorage {
+    static #instance;
+
+    books = [];
+
+    static {
+        this.#instance = new BookStorage();
+    }
+
+    static getInstance() {
+        return this.#instance;
+    }
+
+    addBook(title, author, publisher, pages, year) {
+        this.books.push(new Book(title, author, publisher, pages, year));
+    }
+
+    displayBooks() {
+        let text = '';
+        for (let book of this.books) {
+            text += `title: ${book.title}, author: ${book.author}`;
+        }
+        updateHTML('bookList', text);
+    }
+
+    //TODO
+    searchBooks(title, author) {
+
+    }
+}
+
+// TODO homework
+
+class Author {
+
+}
+
+class Order {
+
+}
+
+class Publisher {
+
+}
+
+class User {
+
+}
+
+document.getElementById('addBook').addEventListener('click', () => {
+    const title = getInputValue('inputTitle');
+    const publisher = getInputValue('inputPublisher');
+    const pages = getInputValue('inputPages');
+    const author = getInputValue('inputAuthor');
+    const year = getInputValue('inputYear');
+
+    BookStorage.getInstance().addBook(title, author, publisher, pages, year);
+    BookStorage.getInstance().displayBooks();
+});
--- js/utils.js
+++ js/utils.js
@@ -1,83 +1,12 @@
-'use strict';
-
-class Book {
-    constructor(title, author, publisher, pages, year) {
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.pages = pages;
-        this.year = year;
-    }
-}
-
-class BookStorage {
-    static #instance;
-
-    books = [];
-
-    static {
-        this.#instance = new BookStorage();
-    }
-
-    static getInstance() {
-        return this.#instance;
-    }
-
-    addBook(title, author, publisher, pages, year) {
-        this.books.push(new Book(title, author, publisher, pages, year));
-    }
-
-    displayBooks() {
-        let text = '';
-        for (let book of this.books) {
-            text += `title: ${book.title}, author: ${book.author}`;
-        }
-        updateHTML('bookList', text);
-    }
-
-    //TODO
-    searchBooks(title, author) {
-
-    }
-}
-
-// TODO homework
-
-class Author {
-
-}
-
-class Order {
-
-}
-
-class Publisher {
-
-}
-
-class User {
-
-}
-
-function getInputValue(elementId) {
+export function getInputValue(elementId) {
 
     const inputElement = document.getElementById(elementId);
     return inputElement.value;
 }
 
-function updateHTML(elementId, value) {
+export function updateHTML(elementId, value) {
 
     const inputElement = document.getElementById(elementId);
     inputElement.innerHTML = value;
 }
 
-document.getElementById('addBook').addEventListener('click', () => {
-    const title = getInputValue('inputTitle');
-    const publisher = getInputValue('inputPublisher');
-    const pages = getInputValue('inputPages');
-    const author = getInputValue('inputAuthor');
-    const year = getInputValue('inputYear');
-
-    BookStorage.getInstance().addBook(title, author, publisher, pages, year);
-    BookStorage.getInstance().displayBooks();
-});
